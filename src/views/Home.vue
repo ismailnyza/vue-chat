cb424516883dd36892672cdaaab4b2cfc172cd36
nadunys <yashnadun@gmail.com>
2021-08-14 20:04:41 +0530

    feat(message):  message molecule

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,29 @@
 <template>
   <div class="home">
-    <profile-view
-      :userName="'John Doe'"
-      :profileImage="'https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png'"
-    />
+    <profile-view :userName="name" :profileImage="contactImage" />
     <hr />
     <contact
-      :contactName="'John Doe'"
+      :contactName="name"
       :secondaryText="'Last message sent'"
-      :contactImage="'https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png'"
+      :contactImage="contactImage"
+      :timestamp="timestamp"
+    />
+    <hr />
+    <message
+      :contactImage="contactImage"
+      :message="message"
+      :isActive="false"
+      :contactName="name"
       :timestamp="timestamp"
     />
   </div>
-  <app-button> Hello Button </app-button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import ProfileView from "@/components/molecules/ProfileView.vue";
-import AppButton from "@/components/atoms/AppButton.vue";
 import Contact from "@/components/molecules/Contact.vue";
+import Message from "@/components/molecules/Message.vue";
 
 import { getReadableTime } from "@/helpers/time";
 
@@ -29,11 +33,17 @@ export default defineComponent({
   name: "Home",
   components: {
     ProfileView,
-    AppButton,
     Contact,
+    Message,
   },
   data: () => {
-    return { timestamp };
+    return {
+      timestamp,
+      name: "John Doe",
+      contactImage:
+        "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png",
+      message: "This is a message. This can be long and short",
+    };
   },
 });
 </script>
