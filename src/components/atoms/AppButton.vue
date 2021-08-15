7df194d3e66f2c4129edb1e95f9402f7598e4eb7
ismailnyza <53538980+ismailnyza@users.noreply.github.com>
2021-08-16 02:15:52 +0530

    drop down menu

--- src/components/atoms/AppButton.vue
+++ src/components/atoms/AppButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button>
+  <button :style="cssProps">
     <slot></slot>
   </button>
 </template>
@@ -14,6 +14,36 @@ export default defineComponent({
   props: {
     isColored: Boolean,
     size: String as PropType<Size>,
+  },
+  computed: {
+    cssProps() {
+      let fontSize;
+      switch (this.textType) {
+        case "header":
+          fontSize = "1.3rem";
+          break;
+        case "title":
+          fontSize = "0.9rem";
+          break;
+        case "subtitle":
+          fontSize = "0.7rem";
+          break;
+        case "message":
+          fontSize = "0.85rem";
+          break;
+        case "tiny-text":
+          fontSize = "0.7rem";
+          break;
+        default:
+          fontSize = "1rem";
+      }
+
+      return {
+        "--isBold": this.isBold && "bold",
+        "--isShaded": this.isShaded && "#ddd",
+        "--fontSize": fontSize,
+      };
+    },
   },
 });
 </script>
